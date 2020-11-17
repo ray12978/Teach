64b34134a20e058bcb69fb2c9248373b72e393e9
Ray-NB <ray93978@gmail.com>
2020-11-17 17:30:06 +0800

    #Teach 17:30

--- app/src/main/java/com/ray/teach/MainActivity.java
+++ app/src/main/java/com/ray/teach/MainActivity.java
@@ -2,18 +2,30 @@ package com.ray.teach;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    String Tag = "now";
+    private String Tag = "now";
+    private Button button1;
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);     //儲存狀態
+        setContentView(R.layout.activity_main); //設定要連接的頁面(Activity)
+        button1 = findViewById(R.id.button);    //設定物件連接的view
+        TextView textView1 = findViewById(R.id.textView2);
         Log.v(Tag, "onCreate");
         your_function();
+        textView1.setText("0");
+        button1.setOnClickListener(v -> {       //監聽按鈕事件(lambda格式)
+            count++;
+            textView1.setText(String.valueOf(count));
+        });
     }
 
     @Override
