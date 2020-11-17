62ec9eea56ba7b1843c88ee5015b32ff8124860f
Ray-NB <ray93978@gmail.com>
2020-11-17 15:42:15 +0800

    #Teach 15:42

--- app/src/main/java/com/ray/teach/MainActivity.java
+++ app/src/main/java/com/ray/teach/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity {
     String Tag = "now";
     @Override
